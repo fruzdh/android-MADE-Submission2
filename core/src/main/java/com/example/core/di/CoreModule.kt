1d5551e2c0a0a2209d284bdfe274cfb9c74c5b91
fruzdh <abrarnadip123@gmail.com>
2021-02-22 18:29:45 +0700

    core-di

--- core/src/main/java/com/example/core/di/CoreModule.kt
+++ core/src/main/java/com/example/core/di/CoreModule.kt
@@ -1,4 +1,61 @@
 package com.example.core.di
 
-class CoreModule {
+import androidx.room.Room
+import com.example.core.BuildConfig
+import com.example.core.data.MovieRepository
+import com.example.core.data.source.local.LocalDataSource
+import com.example.core.data.source.local.room.MovieDatabase
+import com.example.core.data.source.remote.RemoteDataSource
+import com.example.core.data.source.remote.network.ApiService
+import com.example.core.domain.repository.IMovieRepository
+import com.example.core.utils.AppExecutors
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+val databaseModule = module {
+    factory { get<MovieDatabase>().movieDao() }
+    single {
+        Room.databaseBuilder(
+            androidContext(),
+            MovieDatabase::class.java, "Movie.db"
+        ).fallbackToDestructiveMigration().build()
+    }
+}
+
+val networkModule = module {
+    single {
+        OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .connectTimeout(120, TimeUnit.SECONDS)
+            .readTimeout(120, TimeUnit.SECONDS)
+            .build()
+    }
+    single {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BuildConfig.baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(get())
+            .build()
+        retrofit.create(ApiService::class.java)
+    }
+}
+
+val repositoryModule = module {
+    single {
+        LocalDataSource(get())
+    }
+    single {
+        RemoteDataSource(get())
+    }
+    factory {
+        AppExecutors()
+    }
+    single<IMovieRepository> {
+        MovieRepository(get(), get(), get())
+    }
 }
