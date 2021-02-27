0d16068dd0ec8298fecb49c7c252d763742ea69c
fruzdh <abrarnadip123@gmail.com>
2021-02-27 21:44:57 +0700

    more ui2

--- app/src/main/java/com/example/submission2/ui/fragment/home/HomeFragment.kt
+++ app/src/main/java/com/example/submission2/ui/fragment/home/HomeFragment.kt
@@ -8,17 +8,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.core.data.Resource
+import com.example.core.ui.CenterZoomLayoutManager
 import com.example.core.ui.ItemListMovieAdapter
 import com.example.core.ui.ItemListPosterAdapter
 import com.example.submission2.R
 import com.example.submission2.databinding.FragmentHomeBinding
 import com.example.submission2.ui.fragment.detail.DetailFragment
 import org.koin.android.viewmodel.ext.android.viewModel
-import kotlin.math.abs
-import kotlin.math.min
-
 
 class HomeFragment : Fragment() {
 
@@ -340,30 +337,7 @@ class HomeFragment : Fragment() {
             }
 
             with(binding?.rvSearch) {
-                this?.layoutManager = object : LinearLayoutManager(context, HORIZONTAL, false) {
-                    override fun scrollHorizontallyBy(dx: Int, recycler: RecyclerView.Recycler?, state: RecyclerView.State?): Int {
-                        val scrolled = super.scrollHorizontallyBy(dx, recycler, state)
-                        val mShrinkAmount = 0.5f
-                        val mShrinkDistance = 0.75f
-
-                        val midpoint = width / 2.0f
-                        val d0 = 0.0f
-                        val d1 = mShrinkDistance * midpoint
-                        val s0 = 1.0f
-                        val s1 = 1.0f - mShrinkAmount
-                        for (i in 0 until childCount) {
-                            val child = getChildAt(i)
-                            if (child != null) {
-                                val childMidpoint = (getDecoratedRight(child) + getDecoratedLeft(child)) / 2.0f
-                                val d = min(d1, abs(midpoint - childMidpoint))
-                                val scale = s0 + (s1-s0) * (d-d0) / (d1-d0)
-                                child.scaleX = scale
-                                child.scaleY = scale
-                            }
-                        }
-                        return scrolled
-                    }
-                }
+                this?.layoutManager = CenterZoomLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
                 this?.setHasFixedSize(true)
                 this?.adapter = searchAdapter
             }
--- core/src/main/java/com/example/core/ui/CenterZoomLayoutManager.kt
+++ core/src/main/java/com/example/core/ui/CenterZoomLayoutManager.kt
@@ -1,14 +1,34 @@
 package com.example.core.ui
 
 import android.content.Context
-import android.util.AttributeSet
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlin.math.abs
+import kotlin.math.min
 
-class CenterZoomLayoutManager : LinearLayoutManager {
-    private val mShrinkAmount = 0.5f
-    private val mShrinkDistance = 0.75f
+class CenterZoomLayoutManager(context: Context?, orientation: Int, reverseLayout: Boolean) :
+    LinearLayoutManager(context, orientation, reverseLayout) {
 
-    constructor(context: Context?) : super(context) {}
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes) {}
-    constructor(context: Context?, orientation: Int, reverseLayout: Boolean) : super(context, orientation, reverseLayout) {}
+    override fun scrollHorizontallyBy(dx: Int, recycler: RecyclerView.Recycler?, state: RecyclerView.State?): Int {
+        val scrolled = super.scrollHorizontallyBy(dx, recycler, state)
+        val mShrinkAmount = 0.5f
+        val mShrinkDistance = 0.75f
+
+        val midpoint = width / 2.0f
+        val d0 = 0.0f
+        val d1 = mShrinkDistance * midpoint
+        val s0 = 1.0f
+        val s1 = 1.0f - mShrinkAmount
+        for (i in 0 until childCount) {
+            val child = getChildAt(i)
+            if (child != null) {
+                val childMidpoint = (getDecoratedRight(child) + getDecoratedLeft(child)) / 2.0f
+                val d = min(d1, abs(midpoint - childMidpoint))
+                val scale = s0 + (s1-s0) * (d-d0) / (d1-d0)
+                child.scaleX = scale
+                child.scaleY = scale
+            }
+        }
+        return scrolled
+    }
 }
